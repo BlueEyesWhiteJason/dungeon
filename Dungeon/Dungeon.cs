ce280ae9dfb3b2f1d64ce6a1254edc379b3d5414
Jason Speet <jason.speet@gmail.com>
2019-09-23 19:37:49 -0400

    first commit

---
+++ Dungeon/Dungeon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dungeon
+{
+    class Dungeon
+    {
+        public int CurrentFloor;
+
+        public void FaceNextMonster(Hero h)
+        {
+
+        }
+    }
+}
---
+++ Dungeon/Hero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dungeon
+{
+    class Hero
+    {
+        public int Level;
+        public int Attack;
+        public int HP;
+        public int XP;
+
+        public void GainXP(int expGiven)
+        {
+            this.XP += expGiven;
+            if (this.XP > this.Level * 10)
+            {
+                LevelUp();
+            }
+        }
+
+        public void LevelUp()
+        {
+            this.Level++;
+            //randomly increase attack and hp
+            this.XP = 0;
+        }
+
+        public bool IsAlive()
+        {
+            if (this.HP > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
